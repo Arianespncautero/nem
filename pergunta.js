ba5837629ffb4392899efc5eae3fd834f51d1a3b
Arianespncautero <105888302+Arianespncautero@users.noreply.github.com>
2024-09-17 10:58:42 -0300

    sd

---
+++ pergunta.js
@@ -0,0 +1,128 @@
+criaCartao(
+    'Química',
+    'O que é um mol?',
+    'Mol é uma unidade de medida');
+    
+    
+    
+    
+    criaCartao(
+    'Física',
+    'Qual a condição para que haja eco?',
+    'Haver um obstaculo a mais de 17m de distancia');
+    
+    
+    
+    
+    criaCartao(
+    'Química',
+    'O que é copolimerização?',
+    'União de várias moléculas idênticas para formar uma nova molécula maior a partir de uma mistura de dois ou vários monómeros');
+    
+    
+    
+    criaCartao(
+    'Biologia',
+    'O que é uma hemacia?',
+    ' são as células mais numerosas do sangue, cuja forma é de disco bicôncavo com espessura maior da margem (2,6 µm) e espessura menor no centro (0,8 µm). ');
+    
+    
+    
+    criaCartao(
+    'Biologia',
+    'O que é uma sinapse?',
+    ' folhas modificadas presentes no interior de flores ');
+    
+    
+    criaCartao(
+    'Biologia',
+    'O que é um carpelo?',
+    'resposta');
+    
+    
+    criaCartao(
+    'Histotia',
+    'O foi a revoluçao industrial?',
+    'foi o período de grande desenvolvimento tecnológico que teve início na Inglaterra a partir da segunda metade do século XVIII e que se espalhou pelo mundo, causando grandes transformações ');
+    
+    
+    criaCartao(
+    'Matematica',
+    'Há um pato entre dois patos, um pato atrás de um pato e um pato na frente de outro pato. De quantos patos estamos falando?',
+    '3 patos');
+    
+    
+    criaCartao(
+    'Matematica',
+    Há 7 passarinhos em um galho de árvore. Um menino atira em um deles, quantos passarinhos sobraram no galho?',
+    ' Nenhum, porque os outros seis fugiram assustados.');
+    
+    
+    criaCartao(
+    'Fisica',
+    'Alonso decidiu passear pelas cidades próximas da região onde mora. Para conhecer os locais, ele gastou 2 horas percorrendo uma distância de 120 km. Que velocidade Alonso estava em seu passeio?',
+    'C)60km/h');
+    
+    
+    criaCartao(
+    'Geopolitica',
+    ' Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Goulart');
+    
+    criaCartao(
+    'Historio',
+    'Quais paises faziam parte da triplice entente?',
+    'França, inglaterra e Rússia');
+    
+    
+    criaCartao(
+    'Portugues',
+    'Em qual das alternativas os temos em destaque são objeto direto?',
+    'B)comprei o lanche.');
+    
+    
+    criaCartao(
+    'Geopolitica',
+    'O que foi o tratado de Versalhes?',
+    'Tratado que sentenciou a Alemanha após a Primeira Guerra Mundial'),
+    
+    
+    criaCartao(
+    'Física',
+    'Como passar de km/h para m/s',
+    'Divida k valor em km/h por 3,6');
+    
+    
+    
+    criaCartao(
+    'Matematica',
+    'O que pode substituir f(x)',
+    'Y');
+    
+    
+    
+    criaCartao(
+    'Matematica',
+    'Qual a formula de uma funçao afim?',
+    'F(x) = ax + b');
+    
+    
+    
+    criaCartao(
+    'Historia',
+    'Como foi chamado o governo de getulio vargas entre 1937-45?',
+    'Estado novo');
+    
+    
+    
+    criaCartao(
+    'Biologia',
+    'O que sao genes alelos',
+    'Os genes alelos sao aqueles que ocupam o mesmo locus em cromossomos homólogos e estão envolvidos na determinação de um mesmo caráter');
+    
+    
+    
+    criaCartao(
+    'Fisica',
+    'Qual é a formula da primeira lei da temodinamica',
+    ' ΔU = Q - W');
