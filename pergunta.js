9d498e4b1a249067df2451e25df842ccd226435f
Arianespncautero <105888302+Arianespncautero@users.noreply.github.com>
2024-09-24 09:32:37 -0300

    .

--- pergunta.js
+++ pergunta.js
@@ -1,128 +1,209 @@
 criaCartao(
+
     'Química',
+    
     'O que é um mol?',
+    
     'Mol é uma unidade de medida');
     
     
     
     
+    
     criaCartao(
+    
     'Física',
+    
     'Qual a condição para que haja eco?',
-    'Haver um obstaculo a mais de 17m de distancia');
+    
+    'Haver um obstáculo a mais de 17 m de distância');
+    
     
     
     
     
     criaCartao(
+    
     'Química',
+    
     'O que é copolimerização?',
+    
     'União de várias moléculas idênticas para formar uma nova molécula maior a partir de uma mistura de dois ou vários monómeros');
     
     
     
+    
     criaCartao(
+    
     'Biologia',
-    'O que é uma hemacia?',
+    
+    'O que é uma hemácia?',
+    
     ' são as células mais numerosas do sangue, cuja forma é de disco bicôncavo com espessura maior da margem (2,6 µm) e espessura menor no centro (0,8 µm). ');
     
     
     
+    
     criaCartao(
+    
     'Biologia',
+    
     'O que é uma sinapse?',
+    
     ' folhas modificadas presentes no interior de flores ');
     
     
+    
     criaCartao(
+    
     'Biologia',
+    
     'O que é um carpelo?',
-    'resposta');
     
+    'folhas modificadas no interior das flores ');
     
-    criaCartao(
-    'Histotia',
-    'O foi a revoluçao industrial?',
-    'foi o período de grande desenvolvimento tecnológico que teve início na Inglaterra a partir da segunda metade do século XVIII e que se espalhou pelo mundo, causando grandes transformações ');
     
     
     criaCartao(
-    'Matematica',
-    'Há um pato entre dois patos, um pato atrás de um pato e um pato na frente de outro pato. De quantos patos estamos falando?',
-    '3 patos');
     
+    'História',
     
-    criaCartao(
-    'Matematica',
-    'Há 7 passarinhos em um galho de árvore. Um menino atira em um deles, quantos passarinhos sobraram no galho?',
-    ' Nenhum, porque os outros seis fugiram assustados.');
+    'O foi a revolução industrial?',
+    
+    'foi o período de grande desenvolvimento tecnológico que teve início na Inglaterra a partir da segunda metade do século XVIII e que se espalhou pelo mundo, causando grandes transformações' );
+    
     
     
     criaCartao(
-    'Fisica',
-    'Alonso decidiu passear pelas cidades próximas da região onde mora. Para conhecer os locais, ele gastou 2 horas percorrendo uma distância de 120 km. Que velocidade Alonso estava em seu passeio?',
-    'C)60km/h');
+    
+    'Matemática',
+    
+    'quais as unidades de medidas no sistema internaciona?' ,
+    
+    'metro, segundo, ampere, kelvin quilograma, candela e mol. ');
+    
     
     
     criaCartao(
-    'Geopolitica',
+    
+    'Matemática',
+    
+    'o que é permutação? ',
+    
+    ' A permutação é uma técnica de contagem utilizada para determinar quantas maneiras existem para ordenar os elementos de um conjunto finito. ');
+    
+    
+    
+    criaCartao(
+    
+    'Física',
+    
+    'qual a função de um resistor?',
+    
+     ' limitar o fluxo de cargas elétricas por meio da conversão da energia elétrica em energia térmica');
+    
+    
+    criaCartao(
+    
+    'Geopolítica',
+    
     ' Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
-    'João Goulart');
+    
+    'João Goulart ');
+    
+    
     
     criaCartao(
-    'Historio',
-    'Quais paises faziam parte da triplice entente?',
+    
+    'Histórico',
+    
+    'Quais países fazem parte da tríplice entente?',
+    
     'França, inglaterra e Rússia');
     
     
+    
     criaCartao(
+    
     'Portugues',
-    'Em qual das alternativas os temos em destaque são objeto direto?',
-    'B)comprei o lanche.');
     
+    'quando se usa a vírgula?',
     
-    criaCartao(
-    'Geopolitica',
-    'O que foi o tratado de Versalhes?',
-    'Tratado que sentenciou a Alemanha após a Primeira Guerra Mundial'),
+    '  é utilizada para separar elementos ou locuções explicativas ou corretivas ');
+    
     
     
     criaCartao(
+    
+    'Geopolítica',
+    
+    'o que foi o tratado de tordesilhas? ',
+    
+    'O Tratado de Tordesilhas foi um acordo firmado entre Portugal e Espanha, em 1494, no intuito de demarcar os limites de exploração dos dois reinos no Atlântico Sul.');
+    
+    
+    
+    criaCartao(
+    
     'Física',
-    'Como passar de km/h para m/s',
-    'Divida k valor em km/h por 3,6');
+    
+    'Como passar de km/h para m/s?' ,
+    
+    'Divida o valor em km/h por 3,6');
+    
     
     
     
     criaCartao(
-    'Matematica',
-    'O que pode substituir f(x)',
+    
+    'Matemática',
+    
+    'O que pode substituir f(x)?',
+    
     'Y');
     
     
     
+    
     criaCartao(
-    'Matematica',
-    'Qual a formula de uma funçao afim?',
+    
+    'Matemática',
+    
+    'Qual a fórmula de uma função afim?',
+    
     'F(x) = ax + b');
     
     
     
+    
     criaCartao(
-    'Historia',
-    'Como foi chamado o governo de getulio vargas entre 1937-45?',
+    
+    'História',
+    
+    'Como foi chamado o governo de Getúlio Vargas entre 1937-45?',
+    
     'Estado novo');
     
     
     
+    
     criaCartao(
+    
     'Biologia',
-    'O que sao genes alelos',
-    'Os genes alelos sao aqueles que ocupam o mesmo locus em cromossomos homólogos e estão envolvidos na determinação de um mesmo caráter');
+    
+    'O que são genes alelos?',
+    
+    'Os genes alelos são aqueles que ocupam o mesmo locus em cromossomos homólogos e estão envolvidos na determinação de um mesmo caráter');
+    
     
     
     
     criaCartao(
-    'Fisica',
-    'Qual é a formula da primeira lei da temodinamica',
+    
+    'Física',
+    
+    'Qual é a fórmula da primeira lei da termodinâmica?',
+    
     ' ΔU = Q - W');
+    
+    
